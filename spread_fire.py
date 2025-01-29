d2a5d25fc7243681e4997064a21828560aceaaa9
Layanagu03 <liana.gu.03@gmail.com>
2025-01-29 22:18:15 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -7,8 +7,18 @@ def spread_fire(grid):
     update_grid = copy.deepcopy(grid)
     for i in range(grid_size-1):
         for j in range(grid_size-1):
-            if grid[i][j] == 1:
-                neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]]
+                if grid[i][j] == 1:
+                    neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]]
+                
+        
+                if i > 0:  # Check above
+                    neighbors.append(grid[i-1][j])
+                if i < grid_size - 1:  # Check below
+                    neighbors.append(grid[i+1][j])
+                if j > 0:  # Check left
+                    neighbors.append(grid[i][j-1])
+                if j < grid_size - 1:  # Check right
+                    neighbors.append(grid[i][j+1])    
                 if 2 in neighbors:
                     update_grid[i][j] = 2
 
